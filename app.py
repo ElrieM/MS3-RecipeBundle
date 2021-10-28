b7ed0d82a617bfe1200d5ead9afa33749e612399
Elrie <79701587+ElrieM@users.noreply.github.com>
2021-10-29 00:33:12 +0200

    docs: repair python installation and deploy to Heroku

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, flash, render_template, 
+    Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
