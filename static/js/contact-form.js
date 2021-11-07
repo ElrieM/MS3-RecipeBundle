da735952fd513a84956737eb6fc1881569f45ff6
Elrie <79701587+ElrieM@users.noreply.github.com>
2021-11-07 22:20:41 +0100

    feat: Create draft contact form and connect to emailJS

---
+++ static/js/contact-form.js
@@ -0,0 +1,22 @@
+/* Adapted from https://jsfiddle.net/13atu8e5/ */
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+  .addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    btn.value = 'Sending...';
+
+    const serviceID = 'default_service';
+    const templateID = 'RecipeBundler';
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
