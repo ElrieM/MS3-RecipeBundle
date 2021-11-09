f960b319f00bd368ba40d1c402190d999a4cf2c8
Elrie <79701587+ElrieM@users.noreply.github.com>
2021-11-10 00:35:12 +0100

    docs: Create add new and remove method step script

--- static/js/adj-recipe.js
+++ static/js/adj-recipe.js
@@ -1,3 +1,5 @@
+
+/* Scripts for adding and removing ingredients */
 function addIngredient() {
     var table = document.getElementById('ingredient-list');
     var rowNum = document.getElementById('ingredient-list').rows.length;
@@ -21,3 +23,26 @@ function delIngredient() {
     }
 }
 
+/* Scripts for adding and removing method steps */
+function addMethod() {
+    var table = document.getElementById('method-list');
+    var rowNum = document.getElementById('method-list').rows.length;
+    var row = table.insertRow(rowNum);
+    var cell = row.insertCell(0);
+
+    var inputStep = document.getElementById('inputMethod').value;
+
+    cell.innerHTML += `${inputStep}`;
+}
+
+function delMethod() {
+    var table = document.getElementById("method-list");
+    var rowNum = table.rows.length;
+    if(rowNum > '1'){
+        var row = table.deleteRow(rowNum - 1);
+        rowNum--;
+    }
+    else {
+        alert('No step to remove')
+    }
+}
