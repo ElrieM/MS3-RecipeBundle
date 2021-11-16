c6cd0bff7d90d1d5d425ccfa5bf8dfced3a03f96
Elrie <79701587+ElrieM@users.noreply.github.com>
2021-11-17 00:22:14 +0100

    docs: Restructuring and splitting files - updating app.py and init_.py files in main folders

--- recipe_bundle/__init__.py
+++ recipe_bundle/__init__.py
@@ -5,14 +5,16 @@ from flask_pymongo import PyMongo
 if os.path.exists("env.py"):
     import env
 
-# Create Flask App and variables connecting to MongoDB
+# Create Flask App
 app = Flask(__name__)
 
+# Variables to connect to MongoDB
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
+mongo.init_app(app)
 
 
 def create_app():
@@ -22,14 +24,14 @@ def create_app():
     # Route import
     from recipe_bundle.admin.routes import admin
     from recipe_bundle.auth.routes import auth
-    from recipe_bundle.contact.routes import contact
+    from recipe_bundle.contact.routes import cont
     from recipe_bundle.error.routes import error
     from recipe_bundle.manage.routes import manage
     from recipe_bundle.recipes.routes import recipes
     # Blueprint routes
     app.register_blueprint(admin)
     app.register_blueprint(auth)
-    app.register_blueprint(contact)
+    app.register_blueprint(cont)
     app.register_blueprint(error)
     app.register_blueprint(manage)
     app.register_blueprint(recipes)
