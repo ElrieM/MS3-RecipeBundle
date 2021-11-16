13c5c2eb9f4a6f8306e155574682b2fb25de91f7
Elrie <79701587+ElrieM@users.noreply.github.com>
2021-11-17 00:24:42 +0100

    docs: Restructuring and splitting files - Updating Admin (index.html) routing and create init file

---
+++ recipe_bundle/admin/routes.py
@@ -0,0 +1,14 @@
+from flask import (render_template, Blueprint)
+
+# Create blueprint for admin object
+admin = Blueprint('admin', __name__)
+
+
+@admin.route("/")
+@admin.route("/home")
+def home():
+    """
+    Loads landing / home page
+    :return render_template of admin.html
+    """
+    return render_template("admin/index.html")
