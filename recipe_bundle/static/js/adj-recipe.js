a463ad4015e29015774efd8b5861fd02e4331d24
Elrie <79701587+ElrieM@users.noreply.github.com>
2021-11-17 17:07:37 +0100

    docs: restructure and tidy up recipe ingredient editing script

--- recipe_bundle/static/js/adj-recipe.js
+++ recipe_bundle/static/js/adj-recipe.js
@@ -1,67 +1,41 @@
 /* Scripts for adding and removing ingredients */
 var x = 0;
-const ingrArray = Array();
-var ingJSON = {};
+const ingrArray = [];
+const methodArray = [];
 
 function addIngredient(ingrArray) {
-    var table = document.getElementById('ingredient-list');
-    var rowNum = document.getElementById('ingredient-list').rows.length;
-    var row = table.insertRow(rowNum);
+    var ingTable = document.getElementById('ingredientTable');
+    var ingRowNum = ingTable.rows.length;
+    var ingRow = ingTable.insertRow(ingRowNum);
+    var ingInput = document.getElementById('inputIngred').value;
 
-    var inputIngr = document.getElementById('inputIngred').value;
-
-    row.insertCell(0).innerHTML += `<td class="col-sm-10">${inputIngr}</td>`;
-    row.insertCell(1).innerHTML += `
+    ingRow.insertCell(0).innerHTML += `<td class="col-sm-10">${ingInput}</td>`;
+    ingRow.insertCell(1).innerHTML += `
         <td>
             <input class="btn btn-primary" type="button" id='btn${x}' value="Remove" onclick="delIngredient(this.parentNode)">
         </td>
     `;
 
-console.log(inputIngr);
-
     /* Adds ingredient to array */
-    ingrArray.push(inputIngr);
+    ingrArray.push(ingInput);
     x++;
     
     /* Resets input field */
     document.getElementById('inputIngred').value ="";
-    document.getElementById('ingr-list').value = ingrArray;
 
     console.log(ingrArray);
 }
 
+/* Delete selected ingredient table row and removes from array */
 function delIngredient(selIngred) {
+    /* Index of ingredient row to be deleted */
     var selRow = selIngred.parentNode.rowIndex;
-    console.log(selRow);
 
-    document.getElementById('ingredient-list').deleteRow(selRow);
+    /* Deletes table row */
+    document.getElementById('ingredientTable').deleteRow(selRow);
 
+    /* Updates the array, removing selected row */
     ingrArray.splice(selRow, 1);
-    
-    document.getElementById('ingr-list').value = ingrArray;
-    
+
     console.log(ingrArray);
-}
-
-/* Scripts for adding and removing method steps */
-function addMethod() {
-    var table = document.getElementById('method-list');
-    var rowNum = document.getElementById('method-list').rows.length;
-    var row = table.insertRow(rowNum);
-    var cell = row.insertCell(0);
-
-    var inputStep = document.getElementById('inputMethod').value;
-
-    cell.innerHTML += `${inputStep}`;
-}
-
-function delMethod() {
-    var table = document.getElementById("method-list");
-    var rowNum = table.rows.length;
-    if (rowNum > '1') {
-        var row = table.deleteRow(rowNum - 1);
-        rowNum--;
-    } else {
-        alert('No step to remove')
-    }
 }
