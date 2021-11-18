bb2826058eda99a5ecc03b7f833fa2a436835e18
Elrie <79701587+ElrieM@users.noreply.github.com>
2021-11-18 02:53:37 +0100

    feat: editing ingredients and methods

--- recipe_bundle/static/js/adj-recipe.js
+++ recipe_bundle/static/js/adj-recipe.js
@@ -1,10 +1,10 @@
 /* Scripts for adding and removing ingredients */
 var x = 0;
-const ingrArray = [];
-const methodArray = [];
+var ingrArray = [];
+var methodArray = [];
+var ingTable = document.getElementById('ingredientTable');
 
 function addIngredient(ingrArray) {
-    var ingTable = document.getElementById('ingredientTable');
     var ingRowNum = ingTable.rows.length;
     var ingRow = ingTable.insertRow(ingRowNum);
     var ingInput = document.getElementById('inputIngred').value;
@@ -12,7 +12,8 @@ function addIngredient(ingrArray) {
     ingRow.insertCell(0).innerHTML += `<td class="col-sm-10">${ingInput}</td>`;
     ingRow.insertCell(1).innerHTML += `
         <td>
-            <input class="btn btn-primary" type="button" id='btn${x}' value="Remove" onclick="delIngredient(this.parentNode)">
+        <a class="btn btn-link" type="button" id='btn${x}' onclick="editIngredient(this.parentNode)"><i class="fas fa-edit"></i></a>
+        <a class="btn btn-link" id='btn${x}' onclick="delIngredient(this.parentNode)"><i class="fas fa-trash-alt"></i></a>
         </td>
     `;
 
@@ -35,10 +36,33 @@ function delIngredient(selIngred) {
     var selRow = selIngred.parentNode.rowIndex;
 
     /* Deletes table row */
-    document.getElementById('ingredientTable').deleteRow(selRow);
+    ingTable.deleteRow(selRow);
 
     /* Updates the array, removing selected row */
     ingrArray.splice(selRow, 1);
+
+    console.log(ingrArray);
+}
+
+/* Edit selected ingredient table row */
+function editIngredient(selIngred) {
+    /* Index of ingredient row to be deleted */
+    var selRow = selIngred.parentNode.rowIndex;
+    var ingInput = document.getElementById('inputIngred').value;
+    
+    ingrList = document.getElementById('ingr-list').value;
+    console.log(ingrList);
+    
+    var ingrArray = ingrList.split(";");
+    console.log(selRow);
+    console.log(ingrArray);
+
+    document.getElementById('inputIngred').value = ingrArray[selRow];
+    /* Update value in table row */
+    document.getElementById('ingredientTable');
+
+    /* Updates the array, removing selected row */
+    ingrArray[selRow] = ingInput;
 
     console.log(ingrArray);
 }
@@ -80,6 +104,29 @@ function delMethod(selMethod) {
 
     /* Updates the array, removing selected row */
     methodArray.splice(selRow, 1);
+
+    console.log(methodArray);
+}
+
+/* Edit selected method table row */
+function editMethod(selMethod) {
+    /* Index of method row to be deleted */
+    var selRow = selMethod.parentNode.rowIndex;
+    var metInput = document.getElementById('inputMethod').value;
+    
+    methodList = document.getElementById('method-list').value;
+    console.log(methodList);
+    var methodArray = methodList.split(";");
+    console.log(selRow);
+    console.log(methodArray);
+
+    document.getElementById('inputMethod').value = methodArray[selRow];
+
+    /* Update value in table row */
+    document.getElementById('methodTable');
+
+    /* Updates the array, removing selected row */
+    methodArray[selRow] = metInput;
 
     console.log(methodArray);
 }
