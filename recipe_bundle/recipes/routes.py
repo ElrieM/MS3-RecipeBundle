7f9adca556fd386cb14c181e2e0afc630b8014dc
Elrie <79701587+ElrieM@users.noreply.github.com>
2021-11-18 22:21:40 +0100

    docs: Upload to AWS S3 added

--- recipe_bundle/recipes/routes.py
+++ recipe_bundle/recipes/routes.py
@@ -1,7 +1,8 @@
 from flask import (flash, render_template, redirect,
-                   request, url_for, Blueprint)
+                   request, url_for, Blueprint, session)
 from bson.objectid import ObjectId
 from recipe_bundle import mongo
+from recipe_bundle.util import util
 
 # Create blueprint for recipes object
 recipes = Blueprint('recipes', __name__)
@@ -10,41 +11,61 @@ recipes = Blueprint('recipes', __name__)
 @recipes.route("/")
 @recipes.route("/collection")
 def collection():
+    """
+    Creates recipe collection page, displaying recipes in DB
+    :return: rendertemplate of collection.html
+    """
+    # Check if user is logged in
+    if "user" not in session:
+        return redirect(url_for("auth/signin"))
+    # collects all recipes in collection
     recipes = list(mongo.db.recipes.find())
+    # renders template
     return render_template("recipes/collection.html", recipes=recipes)
 
 
-@recipes.route("/search", methods=["GET", "POST"])
-def search():
-    query = request.form.get("search-col")
-    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
-    return render_template("recipes/collection.html", recipes=recipes)
-
-
-@recipes.route("/select_recipe/<recipe_id>")
-def select_recipe(recipe_id):
+@recipes.route("/view_recipe/<recipe_id>")
+def view_recipe(recipe_id):
     sel_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("recipes/view_recipe.html", recipe=sel_recipe)
 
 
 @recipes.route("/add_recipe", methods=["GET", "POST"])
-def add_recipe():
-    ingr_list = request.form.getlist("ingr-list")
-    method_list = request.form.getlist("method-list")
+def add_recipe() -> object:
+    """
+    Adds recipe to MongoDB with add_recipe form information
+    :return redirect to (recipe) collection page
+    """
+    # Check if a user is signed in
+    if 'user' not in session:
+        return redirect(url_for("auth.signin"))
 
     if request.method == "POST":
-        recipe = {
-            "recipe_name": request.form.get("recipe_name"),
-            "cuisine": request.form.get("cuisine"),
-            "type": request.form.get("mealType"),
-            "diet": request.form.get("diet"),
-            "ingredients": ingr_list,
-            "method": method_list,
-            "total_time": request.form.get("total_time"),
-            "active_time": request.form.get("active_time")
-        }
-        mongo.db.recipes.insert_one(recipe)
-        flash("Recipe added")
+        try:
+            # Store the recipe image file in AWS S3
+            img_url = util.upload_img('recipe_img')
+            ingr_list = request.form.getlist("ingr-list")
+            method_list = request.form.getlist("method-list")
+            # Create recipe object from form
+            recipe = {
+                "recipe_name": request.form.get("recipe_name"),
+                "cuisine": request.form.get("cuisine"),
+                "type": request.form.get("mealType"),
+                "diet": request.form.get("diet"),
+                "ingredients": ingr_list,
+                "method": method_list,
+                "total_time": request.form.get("total_time"),
+                "active_time": request.form.get("active_time"),
+                "recipe_img": img_url,
+                "creator": session["user"]
+            }
+            # Add recipe to MongoDB
+            mongo.db.recipes.insert_one(recipe)
+            flash("Recipe successfully added")
+        except Exception as e:
+            flash("An error has occurred while adding recipe - " +
+                  getattr(e, 'message', repr(e)))
+        # Redirect to view recipe page
         return redirect(url_for("recipes.collection"))
 
     cuisines = mongo.db.cuisines.find().sort("cuisine_name", 1)
@@ -84,3 +105,9 @@ def delete_recipe(recipe_id):
     mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
     flash("Recipe successfully deleted")
     return redirect(url_for("recipes.collection"))
+
+@recipes.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("search-col")
+    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    return render_template("recipes/collection.html", recipes=recipes)
--- recipe_bundle/util/util.py
+++ recipe_bundle/util/util.py
@@ -2,11 +2,15 @@ import os
 from flask import (render_template, request)
 from werkzeug.utils import secure_filename
 import boto3
+from botocore.exceptions import ClientError
+import logging
+from typing import Tuple
 
 if os.path.exists("env.py"):
     import env
 
 
+# AWS variables
 s3_bucket_name = os.environ.get("AWS_BUCKET_NAME")
 s3_bucket_url = os.environ.get("AWS_DOMAIN")
 s3 = boto3.client('s3',
@@ -15,16 +19,27 @@ s3 = boto3.client('s3',
                   )
 
 
-def upload():
-    if request.method == 'POST':
-        img = request.files['file']
-        if img:
-            filename = secure_filename(img.filename)
-            img.save(filename)
-            s3.upload_file(
-                Bucket=s3_bucket_name,
-                Filename=filename,
-                Key=filename
-            )
-            msg = "Image uploaded successfully! "
-    return render_template("recipes/add_recipe.html", msg=msg)
+# Adapted from https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
+def upload_img(file_name, object_name=None):
+    """
+    Upload a file to recipes S3 bucket
+    :param file_name: File to be uploaded
+    :param bucket: Bucket to which uploading
+    :param object_name: S3 object name - if not specified, uses file_name
+    :returns: If successfully uploaded returns True, else False
+    """
+    img = request.files[file_name]
+    img_file = secure_filename(img.filename)
+    # If S3 object_name not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload file to S3 bucket
+    try:
+        s3_client = boto3.client('s3')
+        s3_client.Bucket('s3_bucket_name').put_object(Key=img_file, Body=img)
+    except ClientError:
+        raise Exception("An error occurred when uploading the image")
+
+    img_url = s3_bucket_url + img_file
+    return img_url
